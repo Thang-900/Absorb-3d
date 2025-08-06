adce1fdfce0a7ee307255e9def2d801ba1e0ab1c
Thang-900 <hmanhthang41@gmail.com>
2025-08-06 21:04:15 +0700

    add move code
    thêm vào code di chuyển cho lỗ và camera

--- Absorb-3d/Assets/_MyAsset/Script/Move.cs
+++ Absorb-3d/Assets/_MyAsset/Script/Move.cs
@@ -6,11 +6,20 @@ public class Move : MonoBehaviour
     public float speed;
     private float horizontal;
     private float vertical;
+    private Rigidbody rb;
     // Update is called once per frame
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
     void Update()
     {
         horizontal= Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
-        
+    }
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(horizontal, 0, vertical) * speed * Time.fixedDeltaTime;
+        rb.MovePosition(transform.position + movement);
     }
 }
