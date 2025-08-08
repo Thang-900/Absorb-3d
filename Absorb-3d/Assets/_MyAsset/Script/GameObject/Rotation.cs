011fbd6fae17a088db0c3526971da7ef9e0c445a
Thang-900 <hmanhthang41@gmail.com>
2025-08-08 21:23:21 +0700

    add sample code
    thêm các khung code mẫu

---
+++ Absorb-3d/Assets/_MyAsset/Script/GameObject/Rotation.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class MoveRotation : MonoBehaviour
+{
+    public Rigidbody rb;
+    private Vector3 lastPosition;
+    private Vector3 moveDirection;
+
+    void Start()
+    {
+        lastPosition = rb.transform.position;
+    }
+
+    void Update()
+    {
+        Vector3 currentPosition = rb.transform.position;
+        Vector3 delta = currentPosition - lastPosition;
+
+        if (delta.magnitude > 0.01f)
+        {
+            moveDirection = delta.normalized;
+
+            // Quay object theo hướng di chuyển
+            transform.rotation = Quaternion.LookRotation(moveDirection);
+        }
+
+        lastPosition = currentPosition;
+    }
+}
