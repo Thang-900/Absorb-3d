45502c0f63f8e473ddeca35b6347715f49198c18
Thang-900 <hmanhthang41@gmail.com>
2025-09-22 21:25:32 +0700

    fixed ui

---
+++ Absorb-3d/Assets/_MyAsset/Script/UI Logic/TalentTree/UpdateScale.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateScale : MonoBehaviour
+{
+    public Image imgs;  // Gán Image này từ Inspector (kéo từ Hierarchy vào)
+
+    private RectTransform rt;
+
+    public float upperHight;
+
+    int count = 0;
+
+    void Start()
+    {
+        // Lấy RectTransform của ảnh khi bắt đầu
+        rt = imgs.GetComponent<RectTransform>();
+    }
+
+    // Hàm gọi để tăng chiều cao
+    public void UpTalentTree()
+    {
+        float currentHeight = rt.sizeDelta.y;
+        if (count<1)
+        {
+            count++;
+            rt.sizeDelta = new Vector2(rt.sizeDelta.x, currentHeight + 160);
+        }
+        else
+        {
+            rt.sizeDelta = new Vector2(rt.sizeDelta.x, currentHeight + upperHight);
+        }
+       
+    }
+}
