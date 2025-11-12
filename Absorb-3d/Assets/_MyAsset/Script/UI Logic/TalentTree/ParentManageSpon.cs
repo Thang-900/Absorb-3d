240a6048a8fb16703129a1552d569a7b62fcda26
Thang-900 <hmanhthang41@gmail.com>
2025-11-12 17:16:28 +0700

    TalentTree Not Finish

---
+++ Absorb-3d/Assets/_MyAsset/Script/UI Logic/TalentTree/ParentManageSpon.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ParentManageSpon : MonoBehaviour
+{
+    public GameObject panelPrefab; // Prefab panel UI
+    private GameObject currentPanel;
+
+    // Hàm này sẽ được gọi khi một button con được nhấn
+    public void SpawnPanelAboveButton(RectTransform buttonRT, string price, Vector2 offset)
+    {
+        // Xóa panel cũ nếu có
+        if (currentPanel != null)
+            Destroy(currentPanel);
+
+        // Instantiate panel mới làm con của parent (cha)
+        currentPanel = Instantiate(panelPrefab, transform); // 'transform' là cha của các button
+        RectTransform panelRT = currentPanel.GetComponent<RectTransform>();
+
+        // Đặt vị trí panel dựa vào vị trí button + offset
+        panelRT.anchoredPosition = buttonRT.anchoredPosition + offset;
+        panelRT.localScale = Vector3.one;
+
+        // Gán text giá nếu có
+        Text txt = currentPanel.GetComponentInChildren<Text>();
+        if (txt != null)
+            txt.text = price;
+    }
+}
---
+++ Absorb-3d/Assets/_MyAsset/Script/UI Logic/TalentTree/UpTallentTree.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpTallentTree : MonoBehaviour
+{
+    public int cost;
+    public Text costText;
+
+
+}
